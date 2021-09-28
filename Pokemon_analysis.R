9fe8f588d37b9649aea4c16698775a1b09c88023
StevenK <2640948k@student.gla.ac.uk>
2021-09-28 11:43:37 +0100

    Nothing
    good

--- Pokemon_analysis.R
+++ Pokemon_analysis.R
@@ -13,6 +13,5 @@ pok_age_l100
 # However, according to the article, the records where the answer of 
 # ATTENTION_filter1 is not "Disagree" should be filtered
 pok_cleaned <- pok[pok$ATTENTION_filter1=="Disagree",]
-pok_cleaned$ATTENTION_filter1 <- NULL
 summary(pok_cleaned)
 
