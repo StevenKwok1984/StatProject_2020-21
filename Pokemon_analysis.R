9311061cbfa72fec5da3f35436bad1854216886d
StevenK <2640948k@student.gla.ac.uk>
2021-10-05 11:23:58 +0100

    Data Grouping
    Form a data set with grouped data

--- Pokemon_analysis.R
+++ Pokemon_analysis.R
@@ -1,5 +1,7 @@
 # import library required
 library(dplyr)
+library(psych)
+
 
 # The first observation
 load("pokemon.Rdata")
@@ -16,14 +18,54 @@ pok_age_l100
 # However, according to the article, the records where the answer of 
 # ATTENTION_filter1 is not "Disagree" should be filtered
 pok_new <- pok[pok$ATTENTION_filter1=="Disagree",]
+pok_new$ATTENTION_filter1 <- NULL
 summary(pok_new)
 
 
-################
-###Regression###
-################
+
+#################################
+###Preparation of Grouped data###
+#################################
 
 ###convert variables into numeric values###
-pok_new[,4:31] <- pok_new[,4:31] %>% mutate_if(is.factor,as.numeric)
+pok_new[,4:30] <- pok_new[,4:30] %>% mutate_if(is.factor,as.numeric)
 summary(pok_new)
 
+###Justify the grouping###
+
+# Calculate Cronbach's Alpha
+# Attitude
+alpha(pok_new[7:12], check.keys=TRUE)
+# steps Attitude
+alpha(pok_new[13:18], check.keys=TRUE)
+# Behaviour
+alpha(pok_new[19:24], check.keys=TRUE)
+# Pokemon Behviour
+alpha(pok_new[27:29], check.keys=TRUE)
+
+#@ Since alpha of behaviour and Pokemon behaviour are "Acceptable"
+#@ we group them together
+
+# variable grouping
+Attitude <- rowMeans(pok_new[7:12])
+StepAttitude <- rowMeans(pok_new[13:18])
+Behaviour <- rowMeans(pok_new[19:24])
+PokemonBehaviour <- rowMeans(pok_new[27:29])
+
+# create new dataset
+Pok_Grouped <- pok_new[c(1:6)]
+Pok_Grouped$Attitude <- Attitude
+Pok_Grouped$StepsAttitude <- StepAttitude
+Pok_Grouped$Behaviour <- Behaviour
+Pok_Grouped$PokemonGo_AppUsage <- pok_new$app_usage_PokemonGoApp_pokemonusage1
+Pok_Grouped$social_sharing <- pok_new$social_sharing
+Pok_Grouped$PokemonRelate_Behaviour <- PokemonBehaviour
+head(Pok_Grouped)
+
+
+########################
+###Model Constructing###
+########################
+
+###Simple linear model###
+Pokeon_Linear <- lm()
