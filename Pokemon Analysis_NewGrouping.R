bdc3e974b07b0c8fc8706135fe51af62beb833c8
StevenK <2640948k@student.gla.ac.uk>
2021-10-21 10:31:06 +0100

    New Goruping
    Discovered that there are some new grouping method

--- Pokemon Analysis_NewGrouping.R
+++ Pokemon Analysis_NewGrouping.R
@@ -53,15 +53,13 @@ alpha(pok_new[27:29], check.keys=TRUE)
 #@ we group them together
 
 # variable grouping
-Attitude <- rowMeans(pok_new[7:12])
-StepsAttitude <- rowMeans(pok_new[13:18])
+Attitude <- rowMeans(pok_new[7:18])
 Behaviour <- rowMeans(pok_new[19:24])
 PokemonBehaviour <- rowMeans(pok_new[27:29])
 
 # create new dataset
 Pok_Grouped <- pok_new[c(4:6)]
 Pok_Grouped$Attitude <- Attitude
-Pok_Grouped$StepsAttitude <- StepsAttitude
 Pok_Grouped$PhyscialActivity <- Behaviour
 Pok_Grouped$PokemonGo_AppUsage <- pok_new$app_usage_PokemonGoApp_pokemonusage1
 Pok_Grouped$social_sharing <- pok_new$social_sharing
@@ -114,7 +112,6 @@ dev.off()
 # full model construction
 Pok.Linear <- glm(PhyscialActivity ~ .^2, data = Pok_Grouped)
 ## variable selection
-final_ols <- ols_step_best_subset(Pok.Linear)
 # use multiple for discovering best model
 Selected_Pok.Linear <- stepAIC(Pok.Linear)
 # model observation
@@ -157,7 +154,7 @@ dev.off()
 ###Gamma###
 
 Pok.Gamma <- glm(PhyscialActivity ~ ., family = Gamma(link="identity"), 
-                           data=Pok_Grouped)
+                 data=Pok_Grouped)
 ## variable selection
 # use multiple for discovering best model
 final_ols <- ols_step_best_subset(Pok.Gamma)
@@ -176,7 +173,7 @@ dev.off()
 
 # full model construction
 Pok.invGaussian <- glm(PhyscialActivity ~ ., family=inverse.gaussian(link="identity")
-                  , data = Pok_Grouped)
+                       , data = Pok_Grouped)
 summary(Pok.invGaussian)
 
 #assumption checking
@@ -208,105 +205,10 @@ ggpairs(Pok.Grouped_New)
 
 par(mfrow = c(2, 3))
 for(i in 1:7){
-        if(i != 2)
-                boxplot(Pok.Grouped_New[,i]~education, data=Pok.Grouped_New)
+  if(i != 2)
+    boxplot(Pok.Grouped_New[,i]~education, data=Pok.Grouped_New)
 }
 for(i in 1:7){
-        if(i != 3)
-                boxplot(Pok.Grouped_New[,i]~Gender, data=Pok.Grouped_New)
+  if(i != 3)
+    boxplot(Pok.Grouped_New[,i]~Gender, data=Pok.Grouped_New)
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-###log-transformation###
-
-# response variable
-logR.model <- lm(log(PhyscialActivity) ~ log(age) + log(education) + log(Gender) + log(Attitude) +
-                         log(StepsAttitude) + log(PokemonGo_AppUsage) + log(PokemonRelate_Behaviour) + 
-                         log(social_sharing), data = Pok_Grouped)
-
-final_ols <- ols_step_best_subset(logR.model)
-final_ols
-final.model <- stepAIC(logR.model)
-summary(final.model)
-par(mfrow = c(2, 2))
-plot(final.model )
-
-###log10-transformation###
-
-# response variable
-logR.model <- lm(log(PhyscialActivity) ~ log10(age) + log10(education) + log10(Gender) +
-                         log10(Attitude) + log10(StepsAttitude) +
-                         log10(PokemonGo_AppUsage) + log10(PokemonRelate_Behaviour) + 
-                         log10(social_sharing), data = Pok_Grouped)
-
-final_ols <- ols_step_best_subset(logR.model)
-final_ols
-final.model <- stepAIC(logR.model)
-summary(final.model)
-par(mfrow = c(2, 2))
-plot(final.model)
-
-
-
-#############
-###Poisson###
-#############
-
-full_model.Poisson <- glm(PhyscialActivity ~ ., family="poisson", 
-                          data=Pok_Grouped)
-summary(full_model.Poisson)
-par(mfrow = c(2, 2))
-plot(full_model.Poisson)
-
-final_ols <- ols_step_best_subset(full_model.Poisson)
-final_ols
-final.model <- stepAIC(full_model.Poisson)
-summary(final.model)
-par(mfrow = c(2, 2))
-plot(full_model.Poisson)
-
-
-##############
-###Gaussian###
-##############
-full_model.Gaussian <- glm(PhyscialActivity ~ ., family = gaussian(link = "identity"), 
-                           data=Pok_Grouped)
-summary(full_model.Gaussian)
-par(mfrow = c(2, 2))
-plot(full_model.Gaussian)
-
-final_ols <- ols_step_best_subset(full_model.Gaussian)
-final_ols
-final.model <- stepAIC(full_model.Gaussian)
-summary(final.model)
-par(mfrow = c(2, 2))
-plot(final.model)
-
-#library(caret)
-#library(randomForest)
-
-#Pok.RF <- randomForest(PhyscialActivity ~ age + education +
-#                             Gender + Attitude + StepsAttitude +
-#                             PokemonGo_AppUsage +
-#                             PokemonRelate_Behaviour, data = Pok_Grouped)
-
-
-#library(e1071)
-#Pok.svm <- svm(PhyscialActivity ~ ., data = Pok_Grouped, kernel = "linear", 
-#               cost = 10, scale = FALSE)
-#set.seed (1)
-#tune.out = tune(svm , PhyscialActivity~., data=Pok_Grouped ,kernel ="linear", 
-#              ranges =list(cost=c(0.001,0.01,0.1, 1,5,10,100)))
-#summary(tune.out)
-#bestmod = tune.out$best.model
